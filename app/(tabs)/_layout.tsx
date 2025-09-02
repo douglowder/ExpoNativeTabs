ae31ed13bff963cde1e8ef5bdb10937b1bfbf590
Douglas Lowder <douglowder@mac.com>
2025-09-02 12:40:32 -0700

    Support web, add TV event demo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,50 +1,5 @@
-import React from 'react';
+import TabLayout from '../../layouts/TabLayout';
 
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-import { withLayoutContext } from 'expo-router';
-import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation';
-
-export const Tabs = withLayoutContext(
-  createNativeBottomTabNavigator().Navigator,
-);
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: () =>
-            require('../../assets/images/home-icon-silhouette.svg'),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: () => ({
-            uri: 'https://www.svgrepo.com/show/445112/explore-solid.svg',
-          }),
-        }}
-      />
-      <Tabs.Screen
-        name="video"
-        options={{
-          title: 'Video',
-          tabBarIcon: () => ({
-            uri: 'https://www.svgrepo.com/show/445112/explore-solid.svg',
-          }),
-        }}
-      />
-    </Tabs>
-  );
+export default function PlatformTabLayout() {
+  return <TabLayout />;
 }
---
+++ layouts/TabLayout.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+import { withLayoutContext } from 'expo-router';
+import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation';
+
+export const Tabs = withLayoutContext(
+  createNativeBottomTabNavigator().Navigator,
+);
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: () => require('@/assets/bar_icons/home.png'),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: () => require('@/assets/bar_icons/code.png'),
+        }}
+      />
+      <Tabs.Screen
+        name="tv_focus"
+        options={{
+          title: 'TV event demo',
+          tabBarIcon: () => require('@/assets/bar_icons/tv.png'),
+        }}
+      />
+      <Tabs.Screen
+        name="video"
+        options={{
+          title: 'Video',
+          tabBarIcon: () => require('@/assets/bar_icons/video.png'),
+        }}
+      />
+    </Tabs>
+  );
+}
