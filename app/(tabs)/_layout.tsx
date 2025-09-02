b2aad657f7aba0e7751f9009f5ff5265ca5d36aa
Douglas Lowder <douglowder@mac.com>
2025-09-02 01:07:55 -0700

    Video demo tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-import { withLayoutContext } from "expo-router";
-import { createNativeBottomTabNavigator } from "@bottom-tabs/react-navigation";
+import { withLayoutContext } from 'expo-router';
+import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation';
 
 export const Tabs = withLayoutContext(
-  createNativeBottomTabNavigator().Navigator
+  createNativeBottomTabNavigator().Navigator,
 );
 
 export default function TabLayout() {
@@ -17,18 +17,29 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: () => require('../../assets/images/home-icon-silhouette.svg') 
+          tabBarIcon: () =>
+            require('../../assets/images/home-icon-silhouette.svg'),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
+          tabBarIcon: () => ({
+            uri: 'https://www.svgrepo.com/show/445112/explore-solid.svg',
+          }),
+        }}
+      />
+      <Tabs.Screen
+        name="video"
+        options={{
+          title: 'Video',
           tabBarIcon: () => ({
             uri: 'https://www.svgrepo.com/show/445112/explore-solid.svg',
           }),
